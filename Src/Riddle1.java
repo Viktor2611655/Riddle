d9081f105bb62a673ee42efd6f8062ee4a6c5964
Viktor2611625 <vstepanenko840@gmail.com>
2023-06-07 23:15:26 +0100

    Riddle11

--- Src/Riddle1.java
+++ Src/Riddle1.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class Riddle1 {
     public static void main(String[] args){
-        int[] x = {0,1,2,3,4,5,6,7,8,9,10};
+        int[] figgures = {0,1,2,3,4,5,6,7,8,9,10};
         Random random = new Random();
-        int sum = random.nextInt(x.length);
+        int sum = random.nextInt(figgures.length);
 
         System.out.println("Угадай число от 0 до 10");
         Scanner scanner = new Scanner(System.in);
